c67af5f5e2d933287182af832e024f8661a27bde
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-09-27 18:09:06 +0100

    refactoring ftp code

--- export_to_ftp.js
+++ export_to_ftp.js
@@ -1,29 +1,25 @@
-// main.js
 require('dotenv').config();
 const { WooCommerce } = require('./config/config');
 const { parsedOrders, parsedProducts, exportOrders, mapData } = require('./dataParser/dataParser');
 const { sendToFTP } = require('./ftpHandler/ftpHandler');
 
-//Data Store
-let data = [];
+async function fetchData() {
+	try {
+		const sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
+		const response = await WooCommerce.get("orders", { after: sixHoursAgo.toISOString() });
+		const data = response.data;
 
-//We subtract 6 hours (in milliseconds) from the current date and time.
-const sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
-
-WooCommerce.get("orders", { after: sixHoursAgo.toISOString() })
-	.then((response) => {
-		data = response.data;
-	})
-	.then(() => {
 		const products = parsedProducts(data);
-		exportOrders(products, "products");
+		await exportOrders(products, "products");
 
 		const pOrders = parsedOrders(data);
-		exportOrders(pOrders, "orders");
+		await exportOrders(pOrders, "orders");
 
 		sendToFTP("products");
 		sendToFTP("orders");
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchData();
